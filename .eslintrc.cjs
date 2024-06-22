58c6b3408b903f92e654eb38673d2c620ac8417d
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-06-22 22:53:13 +0900

    feat: prettier 및 eslint 로 코드 자동화 완료

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,47 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
   },
-}
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: [
+    "react",
+    "react-hooks",
+    "jsx-a11y",
+    "prettier",
+    "@typescript-eslint",
+  ],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    "react/react-in-jsx-scope": "off",
+    "jsx-a11y/click-events-have-key-events": "off", // Disable this rule
+    "jsx-a11y/no-static-element-interactions": "off", // Disable this rule
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
