192b231d3cca985a653cf31c7ebda46411cd54cd
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-06-27 23:40:09 +0900

    feat: 초기세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+import Footer from "./components/Footer/Footer";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={} />
-      </Routes>
-    </Router>
+    <>
+      <Header />
+      <main>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </main>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <>
+      <div>1</div>
+    </>
+  );
+}
+
+export default Home;
