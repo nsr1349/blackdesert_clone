40a6c771f8170b861ef6b774bb58727c36de6ead
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-07-01 23:55:25 +0900

    feat : 3 섹션 만드는 중..

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { homeNews } from "./mock";
 import "./Home.css";
 
@@ -20,20 +21,37 @@ function Home() {
       <section id="news-section">
         {homeNews.map(({ id, image, title }) => (
           <article key={id}>
-            <a href={id}>
+            <Link to={id}>
               <div style={{ backgroundImage: `url('${image}')` }} />
               <h4>{title}</h4>
-            </a>
+            </Link>
           </article>
         ))}
       </section>
+      {/* //////////////////////////////////////////// */}
+      {/* 작업중 */}
       <section id="new-class-section">
         <div className="section-wrap">
-          <img
-            src="https://s1.pearlcdn.com/KR/contents_event/floating/202406/main_new_char/character_dosa_767.png"
-            alt="신규 클래스 배너"
-          />
-          <div></div>
+          <picture>
+            <img
+              src="https://s1.pearlcdn.com/KR/contents_event/floating/202406/main_new_char/character_dosa_767.png"
+              alt="신규 클래스 배너"
+            />
+          </picture>
+          <div className="flex-center">
+            <picture>
+              <img
+                src="https://s1.pearlcdn.com/KR/contents_event/floating/202406/main_new_char/main_title_ko-KR.png"
+                alt="신규 클래스 타이틀"
+                className="flex-center"
+              />
+            </picture>
+            <p>
+              도법과 도술에 통달한 도사는 춤추듯 적을 베어냅니다. <br /> 그
+              움직임에서 피어난 안개는 병사가 되어 전장을 혼란스럽게 만듭니다.
+            </p>
+            <Link to={"/news/dosa"}>더 알아보기</Link>
+          </div>
         </div>
       </section>
     </>
