342140d390fb01a832ec11a70b3a65ca29baad33
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-07-02 22:41:53 +0900

    feat: section 6 목데이터 적용중

--- src/pages/Home/components/ClassList.tsx
+++ src/pages/Home/components/ClassList.tsx
@@ -1,6 +1,24 @@
+// import { useState } from "react";
 import { Link } from "react-router-dom";
 
+const classList = [
+  {
+    name: "워리어",
+    content:
+      "검과 방패를 사용하며 <br />공격과 방어가 적절한 조화를 이루는 근접형 캐릭터",
+    charactorImg:
+      "https://s1.pearlcdn.com/KR/contents/img/portal/main/main_character_class_0.png?v=1",
+    iconImg:
+      "https://s1.pearlcdn.com/KR/contents/img/portal/main/main_character_class_symbol_8.png",
+    xy: {
+      default: { x: "0", y: "0" },
+      active: { x: "0", y: "-11.5rem" },
+    },
+  },
+];
+
 function ClassList() {
+  //   const [selectClass, setSelectClass] = useState(1);
   return (
     <>
       <section id="classlist-section">
@@ -26,23 +44,11 @@ function ClassList() {
             <Link to={"/"}>자세히 알아보기</Link>
             <button>지금 시작하기</button>
           </div>
-          <div className="class-list flex-center">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
+          <ul className="class-list flex-center">
+            {classList.map(({ name, xy }) => (
+              <li key={name} />
+            ))}
+          </ul>
         </div>
       </section>
     </>
